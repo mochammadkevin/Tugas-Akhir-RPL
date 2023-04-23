6afdec0471d08238dcb269bf327b606e3a5f603e
mochammadkevin <mochammadkevin@apps.ipb.ac.id>
2023-04-23 13:47:36 +0700

    happy eid!

--- myFirstApp/admin.py
+++ myFirstApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 from .models import Task
+from .models import Quote
 
 admin.site.register(Profile)
 admin.site.register(Task)
+admin.site.register(Quote)
+
